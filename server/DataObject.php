4f82c0c984deb4cad8a8762f81ba7ae0ca01b7e2
TheRealKS <koen.sauren@gmail.com>
2018-03-26 22:20:39 +0200

    Implemented parts of the server

---
+++ server/DataObject.php
@@ -0,0 +1,40 @@
+<?php
+    class DataObject {
+
+        private static $BASEDATAPATH = "../data/";
+
+        protected $server;
+
+        protected $title; //STRING
+        protected $author; //STRING
+        protected $bio; //JSON
+        protected $images; //ARRAY (OR IF ONLY ONE JUST PATH) OF FILE PATHS
+
+        function __constructor($t, $a, $s) {
+            $this->server = $s;
+            $this->title = $t;
+            $this->author = $a;
+            $this->resolveData();
+        } 
+
+        private function resolveData($title, $author) {
+            $file = $BASEDATAPATH . $title . "_" . $author;
+            if (file_exists($file)) {
+                $rawbio = file_get_contents($file . "/bio.json");
+                $this->bio = json_decode($rawbio);
+                $images = scandir($file . "/images");
+                unset($images[0]);
+                unset($images[1]);
+                $images = array_search($images);
+                if (count($images) == 1) {
+                    $this->images = $images[0];
+                } else {
+                    $this->images = $images;
+                }
+            } else {
+                $s->stderr("Data could not be resolved for object: " . $title . "; " . $author);
+                return;
+            }
+        }
+    }
+?>
---
+++ server/server.php
@@ -0,0 +1,63 @@
+<?php
+    require_once("websocket/websockets.php");
+    
+    class Server extends WebSocketServer {
+
+        protected $users = [];
+        protected $data = [];
+        
+        protected function process($user, $message) {
+            $json = json_decode($message);
+            switch ($json->type) {
+                case 'handshake':
+                    //Process any handshake data here
+                    break;
+                case 'choice':
+                    //User made a choice
+                    
+                    break;
+                default:
+                    //Nothing...
+                    break;
+            }
+        }
+
+        protected function connected($user) {
+            $this->stdout("Connection id: " . $user->id);
+            //Register the new user
+            if (!array_key_exists($user->id, $users)) {
+                $users[$user->id] = $user;
+            }
+        }
+
+        protected function closed($user) {
+            //Remove the user from the system; save their data
+            
+        }
+
+        protected function started() {
+            //List all available data (books)
+            $datapath = "../data";
+            if (file_exists($datapath)) {
+                $files = scandir($datapath);
+                $c = count($files);
+                for ($i = 3; $i < $c; $i++) {
+                    $filename = $files[$i];
+                    $parts = explode("_", $filename);
+                    $obj = new DataObject($parts[0], $parts[1], $this);
+                    array_push($this->data, $obj);
+                }
+            } else {
+                $this->stderr("Data folder not found!");
+            }
+        }
+    }
+
+    $s = new TestServer("0.0.0.0", "6848");
+
+    try {
+        $s->run();
+    } catch (Exception $e) {
+        $s->stdout($e->getMessage());
+    }
+?>
