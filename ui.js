97ac477a2cfd3df485a268071e0c6f8ab41ff720
TheRealKS <koen.sauren@gmail.com>
2018-03-26 19:54:31 +0200

    Swiping is now done

--- ui.js
+++ ui.js
@@ -6,26 +6,30 @@ var topTreshold = 0;
 var bottomTreshold = 0;
 var tilted = false;
 var element;
-window.onload = function() {
+var width, height;
+docReady(function() {
     calculateTresholds();
     element = document.getElementById("card");
     var CardPanRecognizer = new Hammer(element);
     CardPanRecognizer.add(new Hammer.Pan({ direction: Hammer.DIRECTION_ALL, treshold: 0 }));
     CardPanRecognizer.on("pan", HandleDrag);
-}
+});
 
 function calculateTresholds() {
-    var width = window.innerWidth ||
+    width = window.innerWidth ||
         document.documentElement.clientWidth ||
         document.body.clientWidth;
-    var height = window.innerHeight ||
+    height = window.innerHeight ||
         document.documentElement.clientHeight ||
         document.body.clientHeight;
     leftTiltTreshold = (width / -2) + 20;
     leftTreshold = (width / -2) + 10;
     rightTiltTreshold = (width / 2) - 20;
     rightTreshold = (width / 2) - 10;
+    topTreshold = document.getElementById("header").style.bottom;
+    bottomTreshold = height - document.getElementById("footer").style.height;
     console.log(rightTiltTreshold);
+    alert(width / 4 + " ; " + -width / 4);
 }
 
 var lastX = 0;
@@ -36,6 +40,7 @@ var isDragging = false;
 function HandleDrag(event) {
     //Reference to card obj
     let element = event.target;
+    let direction = event.direction;
     //Calculate new position of card
     if (!isDragging) {
         isDragging = true;
@@ -44,6 +49,7 @@ function HandleDrag(event) {
     }
     let eX = (event.deltaX + lastX);
     let eY = (event.deltaY + lastY);
+    //console.log(eX, eY);
     //TODO: add treshold;
     //Move card to new position
     if (eX <= leftTiltTreshold) {
@@ -62,18 +68,29 @@ function HandleDrag(event) {
             tilted = false;
         }
     }
-    let velX = event.velocityX;
-    if (velX > highestVelocity) {
-        highestVelocity = velX;
-        document.getElementById("velocity").innerHTML = velX;
+
+    if (eY <= topTreshold || eY >= bottomTreshold) { /*Botom/toptreshold*/ } else {
+        element.style.left = eX + "px";
+        element.style.top = eY + "px";
     }
-    document.getElementById("velocity").innerHTML = event.velocity;
-    element.style.left = eX + "px";
-    element.style.top = eY + "px";
 
     if (event.isFinal) {
+        console.log(event.deltaX);
+        console.log(event.velocity);
+        document.getElementById("velocity").innerHTML = event.velocity;
+        document.getElementById("velocity").innerHTML += " ; " + event.deltaX;
         isDragging = false;
-        
+        if (event.direction !== Hammer.DIRECTION_VERTICAL) {
+            if (event.velocity > -1 || event.velocity < 1) {
+                if (event.deltaX > width / 4 && event.velocity > 1) {
+                    dismissCard();
+                } else if (event.deltaX < -width / 4 && event.velocity < -1) {
+                    dismissCard();
+                }
+            } else {
+                dismissCard();
+            }
+        }
     }
 }
 
